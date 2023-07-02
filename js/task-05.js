1fa6911b058f4a88fd8e55913ffa9337c6e65412
Roman Kryhul <104628639+UmyaFamilia@users.noreply.github.com>
2023-07-02 15:48:31 +0300

    5 is corect

--- js/task-05.js
+++ js/task-05.js
@@ -2,4 +2,7 @@ const inpute = document.querySelector("#name-input");
 const span = document.querySelector("#name-output");
 inpute.addEventListener("input", () => {
   span.textContent = inpute.value;
+  if (span.textContent === "") {
+    span.textContent = "Anonymous";
+  }
 });
